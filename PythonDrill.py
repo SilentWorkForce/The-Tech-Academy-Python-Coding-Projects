876a872ae8e51eeb0fecf2f9a8b1c9549ca28f26
Christopher <christopherstam@yahoo.com>
2019-03-23 13:39:04 -0700

    Create PythonDrill.py

---
+++ PythonDrill.py
@@ -0,0 +1,28 @@
+import os
+
+files = os.listdir('D:\\PyDrill')
+print(files)
+
+fName = 'drill4.txt'
+
+fPath = 'D:\\PyDrill\\'
+
+abPath = os.path.join(fPath, fName)
+
+print(abPath)
+
+time = os.path.getmtime('D:\PyDrill\drill4.txt')
+print(time)
+
+
+for file in os.listdir('D:\\PyDrill\\'):
+    if file.endswith(".txt"):
+        print(os.path.join,("/PyDrill", file,))
+
+
+path = 'D:\PyDrill'
+name_list = os.listdir(path)
+full_list = [os.path.join(path,i) for i in name_list]
+time_sorted_list = sorted(full_list, key=os.path.getmtime)
+
+print (time_sorted_list)
