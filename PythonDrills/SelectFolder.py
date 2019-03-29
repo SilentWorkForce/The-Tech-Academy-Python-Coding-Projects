c26e85859f8be8bf8bb3043946067ad56a02805a
Christopher <47716553+SilentWorkForce@users.noreply.github.com>
2019-03-29 12:28:20 -0700

    Update SelectFolder.py

--- PythonDrills/SelectFolder.py
+++ PythonDrills/SelectFolder.py
@@ -5,6 +5,7 @@ from tkinter import *
 
 
 class ParentWindow(Frame):
+    global folder_path
     def __init__ (self,master):
         Frame.__init__(self)
 
@@ -16,19 +17,24 @@ class ParentWindow(Frame):
         self.master.config(bg='#fff')
 
 
-        self.btnSubmit = Button(self.master, text='Select Folder', width=12, height=2, command=self.submit)
+        self.btnSubmit = Button(self.master, text='Submit', width=12, height=2, command=self.submit)
         self.btnSubmit.grid(row=0, column=0,padx=(50,0),pady=(50,0),sticky=N+W)
 
         self.txtSearch = Entry(self.master,text='',font=('Helvetica',16),fg='black', bg='#FFF')
         self.txtSearch.grid(row=0, column=1,padx=(50,0),pady=(50,0))
 
+
+
         
     def submit(self):
-        global folder_path
         dir = filedialog.askdirectory()
-        folder_path.set(filename)
-        print(filename)
-        folder_path = 
+##        text = Text(root)
+        self.txtSearch.insert(END, dir)
+        print(dir)
+        
+##        folder_path.set('C:\Users\chris\OneDrive\Desktop\__pycache__\SelectFolder.py')
+##        print('C:\Users\chris\OneDrive\Desktop\__pycache__\SelectFolder.py')
+        
 
 
 
