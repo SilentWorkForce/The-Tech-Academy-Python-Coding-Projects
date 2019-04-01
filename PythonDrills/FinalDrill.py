cc41071fa5314875b7cfd40ccf1fb022be630cf6
Christopher <christopherstam@yahoo.com>
2019-04-01 11:46:52 -0700

    Update FinalDrill.py

--- PythonDrills/FinalDrill.py
+++ PythonDrills/FinalDrill.py
@@ -12,23 +12,18 @@ class ParentWindow(Frame):
     def __init__ (self,master):
         Frame.__init__(self)
 
-
         self.master = master
         self.master.resizable(width=False, height=False)
         self.master.geometry('{}x{}'.format(600,200))
         self.master.title('AskDir')
         self.master.config(bg='#fff')
-
-
+        
         self.btnSubmit = Button(self.master, text='Submit', width=12, height=2, command=self.submit)
         self.btnSubmit.grid(row=0, column=0,padx=(50,0),pady=(50,0),sticky=N+W)
-
         self.btnSelect = Button(self.master, text='Select', width=12, height=2, command=self.select)
         self.btnSelect.grid(row=1, column=0,padx=(50,0),pady=(25,0),sticky=N+W)
-
         self.txtSearch = Entry(self.master,text='',font=('Helvetica',16),fg='black', bg='#FFF')
         self.txtSearch.grid(row=0, column=1,padx=(50,0),pady=(50,0))
-
         self.txtSearch = Entry(self.master,text='',font=('Helvetica',16),fg='black', bg='#FFF')
         self.txtSearch.grid(row=1, column=1,padx=(50,0),pady=(25,0))
 
